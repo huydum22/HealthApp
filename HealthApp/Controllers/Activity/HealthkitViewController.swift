e8764d0e4f03acc3eb9d21442e596646431983ea
queo <queo@queos-mac.local>
2019-01-11 01:34:41 +0700

    update sync data voice health kit

--- HealthApp/Controllers/Activity/HealthkitViewController.swift
+++ HealthApp/Controllers/Activity/HealthkitViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 import HealthKit
 let healthKitStore:HKHealthStore = HKHealthStore()
 class ViewController: UIViewController {
-    var todayActiveEnergy = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,54 +32,73 @@ class ViewController: UIViewController {
         }
     }
     func getTodaysSteps(completion: @escaping (Double) -> Void) {
-        
         let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
         
-        let now = Date()
-        let startOfDay = Calendar.current.startOfDay(for: now)
-        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
-        
-        let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
-            var resultCount = 0.0
-            guard let result = result else {
-                print("Failed to fetch steps rate")
-                completion(resultCount)
-                return
+            let query = HKObserverQuery(sampleType: stepsQuantityType, predicate: nil) { (query, completionHandler, error) in
+               
+             /*   guard let completionHandler = query else {
+                    print("Failed to fetch steps rate")
+                    completion(resultCount)
+                    return
+                }*/
+                let now = Date()
+                let startOfDay = Calendar.current.startOfDay(for: now)
+                let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+                
+                let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+                    var resultCount = 0.0
+                    guard let result = result else {
+                        print("Failed to fetch steps rate")
+                        completion(resultCount)
+                        return
+                    }
+                    if let sum =  result.sumQuantity() {
+                        resultCount = sum.doubleValue(for: HKUnit.count())
+                    }
+                    
+                    DispatchQueue.main.async {
+                        completion(resultCount)
+                    }
+                }
+                
+                healthKitStore.execute(query)
+                
             }
-            if let sum =  result.sumQuantity() {
-                resultCount = sum.doubleValue(for: HKUnit.count())
-            }
+                healthKitStore.execute(query)
+                healthKitStore.enableBackgroundDelivery(for: stepsQuantityType, frequency: .immediate) { (complete, error) in
+                }
             
-            DispatchQueue.main.async {
-                completion(resultCount)
-            }
-        }
-        healthKitStore.execute(query)
     }
     func getTodayCalo(completion: @escaping (Double) -> Void) {
         
         let caloType = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!
         
-        let now = Date()
-        let startOfDay = Calendar.current.startOfDay(for: now)
-        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
-        let query = HKStatisticsQuery(quantityType: caloType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
-            var resultCount = 0.0
-            guard let result = result else {
-                print("Failed to fetch steps rate")
-                completion(resultCount)
-                return
+         let query = HKObserverQuery(sampleType: caloType, predicate: nil) { (query, completionHandler, error) in
+        
+            let now = Date()
+            let startOfDay = Calendar.current.startOfDay(for: now)
+            let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+            let query = HKStatisticsQuery(quantityType: caloType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+                var resultCount = 0.0
+                guard let result = result else {
+                    print("Failed to fetch steps rate")
+                    completion(resultCount)
+                    return
+                }
+                if let sum =  result.sumQuantity() {
+                    resultCount = sum.doubleValue(for: HKUnit.kilocalorie())
+                   
+                }
+                
+                DispatchQueue.main.async {
+                    completion(resultCount)
+                }
             }
-            if let sum =  result.sumQuantity() {
-                resultCount = sum.doubleValue(for: HKUnit.kilocalorie())
-               
-            }
-            
-            DispatchQueue.main.async {
-                completion(resultCount)
-            }
+            healthKitStore.execute(query)
         }
         healthKitStore.execute(query)
+        healthKitStore.enableBackgroundDelivery(for: caloType, frequency: .immediate) { (complete, error) in
+        }
     }
     @IBOutlet weak var totalCalo: UILabel!
     @IBOutlet weak var totalSteps: UILabel!
